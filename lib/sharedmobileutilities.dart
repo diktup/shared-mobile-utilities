1caeea9b6f28bc2b4bba8a9a35179a959e586f63
Houssem Fadhli <houssem.fadhli@loyalcraft.com>
2022-12-29 12:12:53 +0100

    [FRONT] Remove INTL package.

--- lib/sharedmobileutilities.dart
+++ lib/sharedmobileutilities.dart
@@ -2,12 +2,10 @@ library sharedmobileutilities;
 
 // CLASSES
 
-export 'classes/date_utility.dart';
-export 'classes/dotted_border.dart';
-export 'classes/enums.dart';
-export 'classes/locator.dart';
-export 'classes/mix_utility.dart';
 export 'classes/navigation_service.dart';
+export 'classes/dotted_border.dart';
+export 'classes/locator.dart';
+export 'classes/enums.dart';
 
 // CLASSES
 
