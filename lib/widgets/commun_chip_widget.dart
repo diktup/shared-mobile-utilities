7ed3220e29ff8a2798331f585349ae1c400226ed
Houssem Fadhli <houssem.fadhli@loyalcraft.com>
2023-01-05 15:40:20 +0100

    [FRONT] Bug fixes performance improvements.

--- lib/widgets/commun_chip_widget.dart
+++ lib/widgets/commun_chip_widget.dart
@@ -5,25 +5,25 @@ import 'package:flutter/material.dart';
 class CommunChipWidget extends StatelessWidget {
   CommunChipWidget({
     Key? key,
-    required this.borderRadiusGeometry,
-    required this.edgeInsetsGeometry,
-    this.marginEdgeInsetsGeometry,
+    required this.borderRadius,
+    required this.padding,
     required this.color,
     this.onLongPress,
     this.textStyle,
     this.boxShadow,
     this.boxBorder,
     this.gradient,
+    this.margin,
     this.string,
     this.height,
     this.onTap,
     this.child,
     this.width,
   }) : super(key: key);
-  EdgeInsetsGeometry? marginEdgeInsetsGeometry;
-  BorderRadiusGeometry borderRadiusGeometry;
-  EdgeInsetsGeometry edgeInsetsGeometry;
+  BorderRadiusGeometry borderRadius;
   void Function()? onLongPress;
+  EdgeInsetsGeometry? margin;
+  EdgeInsetsGeometry padding;
   List<BoxShadow>? boxShadow;
   void Function()? onTap;
   TextStyle? textStyle;
@@ -45,12 +45,12 @@ class CommunChipWidget extends StatelessWidget {
               : communChipWidget();
 
   Widget communChipWidget() => Container(
-        margin: marginEdgeInsetsGeometry ?? EdgeInsets.zero,
-        padding: edgeInsetsGeometry,
+        margin: margin ?? EdgeInsets.zero,
+        padding: padding,
         height: height,
         width: width,
         decoration: BoxDecoration(
-          borderRadius: borderRadiusGeometry,
+          borderRadius: borderRadius,
           boxShadow: boxShadow ?? [],
           gradient: gradient,
           border: boxBorder,
