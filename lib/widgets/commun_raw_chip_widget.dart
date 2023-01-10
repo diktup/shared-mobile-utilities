2c8508335b309b2789f37b54f40f586029270350
Houssem Fadhli <houssem.fadhli@loyalcraft.com>
2023-01-10 09:40:12 +0100

    [FRONT] Bug fixes performance improvements.

--- lib/widgets/commun_raw_chip_widget.dart
+++ lib/widgets/commun_raw_chip_widget.dart
@@ -5,39 +5,40 @@ class CommunRawChipWidget extends StatelessWidget {
   CommunRawChipWidget({
     Key? key,
     required this.backgroundColor,
+    required this.checkmarkColor,
     required this.selectedColor,
+    required this.showCheckmark,
     required this.onSelected,
     required this.selected,
     required this.avatar,
+    required this.shape,
     required this.label,
   }) : super(key: key);
+  void Function(bool) onSelected;
   Color backgroundColor;
   OutlinedBorder? shape;
   Color? checkmarkColor;
-  TextStyle? labelStyle;
   Color selectedColor;
   bool? showCheckmark;
   bool selected;
   Widget avatar;
-  String label;
+  Widget label;
 
-  void Function(bool) onSelected;
   @override
   Widget build(BuildContext context) => SizedBox(
         height: 34.0,
         child: RawChip(
           selectedShadowColor: Colors.transparent,
-          label: Text(label, style: labelStyle),
           padding: const EdgeInsets.all(8.0),
-          shadowColor: Colors.transparent,
           backgroundColor: backgroundColor,
+          shadowColor: Colors.transparent,
           checkmarkColor: checkmarkColor,
-          selectedColor: selectedColor,
           showCheckmark: showCheckmark,
-          labelStyle: labelStyle,
+          selectedColor: selectedColor,
           onSelected: onSelected,
           selected: selected,
           avatar: avatar,
+          label: label,
           shape: shape,
         ),
       );
